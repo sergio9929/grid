5083ef077b2b95689df713e8166a914e350c58d7
sergio9929 <sergio9929@hotmail.com>
2020-05-02 16:02:46 +0200

    añadido boton de play

--- jsFunctions.js
+++ jsFunctions.js
@@ -1,19 +1,31 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
+        document.getElementsByTagName("video")[i].volume = 0;
         document.getElementsByTagName("video")[i].addEventListener("mouseover", unmute)
         document.getElementsByTagName("video")[i].addEventListener("mouseleave", mute)
-
+        document.getElementsByTagName("video")[i].addEventListener("click", change)
     }
 })
 
 function unmute() {
-    var video = this;
-    video.volume = .4;
-    video.muted = false;
-    
+    setTimeout(() => {
+        this.volume = .4;
+        this.play();
+    }, 10)
 }
+
 function mute() {
-    var video = this;
-    video.muted = true;
-    video.volume = 0;
+    this.pause();
+    this.currentTime = 0;
+    this.volume = 0;
+}
+
+function change() {
+    if (this.paused) {
+        this.volume = .4;
+        this.play();
+    } else {
+        this.pause();
+        this.volume = 0;
+    }
 }
