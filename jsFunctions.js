b7c0a636a7a2b397fdab7e62f81420b438c79e95
sergio9929 <sergio9929@hotmail.com>
2020-05-02 17:48:35 +0200

    pequeño cambio

--- jsFunctions.js
+++ jsFunctions.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
         document.getElementsByTagName("video")[i].volume = 0;
-        document.getElementsByTagName("video")[i].addEventListener("mouseover", unmute)
-        document.getElementsByTagName("video")[i].addEventListener("mouseleave", mute)
-        document.getElementsByTagName("video")[i].addEventListener("click", change)
+        document.getElementsByTagName("video")[i].addEventListener("mouseover", unmute);
+        document.getElementsByTagName("video")[i].addEventListener("mouseleave", mute);
+        document.getElementsByTagName("video")[i].addEventListener("click", change);
     }
 })
 
@@ -15,7 +15,7 @@ function unmute() {
 }
 
 function mute() {
-    this.load();
+    this.pause();
     this.currentTime = 0;
     this.volume = 0;
 }
