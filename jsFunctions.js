4d87435d41ed7faa4dfba3840ee12db96bd7d68a
sergio9929 <sergio9929@hotmail.com>
2020-05-03 23:26:01 +0200

    añadido boton de pause, stop y mp4

--- jsFunctions.js
+++ jsFunctions.js
@@ -1,31 +1,112 @@
+var videoPosition = getOffsetHeight();
+var videoLoaded = true;
+var timer;
 document.addEventListener("DOMContentLoaded", function (event) {
+    var timer;
     for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
         document.getElementsByTagName("video")[i].volume = 0;
-        document.getElementsByTagName("video")[i].addEventListener("mouseover", unmute);
-        document.getElementsByTagName("video")[i].addEventListener("mouseleave", mute);
-        document.getElementsByTagName("video")[i].addEventListener("click", change);
+        document.getElementsByTagName("video")[i].play();
+        document.getElementsByTagName("video")[i].parentElement.addEventListener("mouseover", unmute);
+        document.getElementsByTagName("video")[i].parentElement.addEventListener("mouseleave", mute);
+        document.getElementsByTagName("video")[i].parentElement.addEventListener("click", change);
+        document.addEventListener("scroll", ()=>{
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => { scroll() }, 50);
+        });
+        window.addEventListener("resize", () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => { videoPosition = getOffsetHeight()}, 100);
+        })
     }
-})
+});
+
+function scroll() {
+    if (window.pageYOffset - 100 > videoPosition) {
+        for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
+            if (!document.getElementsByTagName("video")[i].paused) {
+                change1(document.getElementsByTagName("video")[i])
+            }
+        }
+        videoLoaded = false;
+    } else {
+        if (videoLoaded == false) {
+            for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
+                change1(document.getElementsByTagName("video")[i])
+            }
+            videoLoaded = true;
+        }
+    }
+}
 
 function unmute() {
-    setTimeout(() => {
-        this.volume = .4;
-        this.play();
-    }, 10)
+    this.parentElement.getElementsByTagName("video")[0].volume = .4;
 }
 
 function mute() {
-    this.pause();
-    this.currentTime = 0;
-    this.volume = 0;
+    this.parentElement.getElementsByTagName("video")[0].volume = 0;
 }
 
 function change() {
-    if (this.paused) {
-        this.volume = .4;
-        this.play();
+    var video = this.parentElement.getElementsByTagName("video")[0];
+    if (event.detail > 1) {
+        video.currentTime = 0;
+        video.volume = .4;
+        video.parentElement.children[0].className = "play";
+        video.parentElement.children[0].setAttribute("src", "img/play.svg");
+        video.play();
     } else {
-        this.pause();
-        this.volume = 0;
+        if (video.paused) {
+            video.volume = .4;
+            video.parentElement.children[0].className = "play";
+            setTimeout(() => {
+                video.parentElement.children[0].setAttribute("src", "img/play.svg");
+            }, 200)
+
+            video.play();
+
+        } else {
+            video.pause();
+            video.volume = 0;
+            video.parentElement.children[0].setAttribute("src", "img/pause.svg");
+            video.parentElement.children[0].className = "pause";
+
+        }
     }
+}
+
+function change1(video) {
+    if (video.paused) {
+        video.volume = 0;
+        video.parentElement.children[0].className = "play";
+        setTimeout(() => {
+            video.parentElement.children[0].setAttribute("src", "img/play.svg");
+        }, 200)
+        video.play();
+
+    } else {
+        video.pause();
+        video.parentElement.children[0].setAttribute("src", "img/pause.svg");
+        video.parentElement.children[0].className = "pause";
+    }
+}
+
+function getOffsetHeight() {
+    var videoMaxPosition = 0;
+    for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
+        var elem = document.getElementsByTagName("video")[i];
+        var offsetTop = elem.offsetHeight;
+        do {
+            if (!isNaN(elem.offsetTop)) {
+                offsetTop += elem.offsetTop;
+            }
+        } while (elem = elem.offsetParent);
+        if (offsetTop > videoMaxPosition) {
+            videoMaxPosition = offsetTop;
+        }
+    }
+    return videoMaxPosition;
 }
